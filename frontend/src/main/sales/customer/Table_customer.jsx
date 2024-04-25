f3b0154986e5940620c617a7879004b9ddae79e9
Ruiqi Yu <37889326+ruiqiy4-1663775@users.noreply.github.com>
2024-04-25 02:38:51 -0700

    k

--- frontend/src/main/sales/customer/Table_customer.jsx
+++ frontend/src/main/sales/customer/Table_customer.jsx
@@ -27,9 +27,9 @@ function Table({ data, updateTable }) {
     return (
         <div>
             {selectedRow && <UpdateForm updateTable={updateTable} content={selectedRow}
-                clearSelected={() => setSelectedRow(null)} /> }
-                
-                <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                clearSelected={() => setSelectedRow(null)} />}
+
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
@@ -55,7 +55,7 @@ function Table({ data, updateTable }) {
                     </tbody>
                 </table>
             </div>
-            
+
         </div>
     )
 }
